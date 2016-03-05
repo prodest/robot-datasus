017808c4e53c1b02702fc6d0cc1e4238a01e7071
pnakibar <pedro.nakibar@xdevel.com.br>
2016-03-05 19:10:00 -0300

    lendo urls

--- csvreader.js
+++ csvreader.js
@@ -1,9 +1,11 @@
 var csv = require('fast-csv')
 
 exports.processFile = function (fileName, fn) {
-// const fileName = 'Fonte de dados - Municipios do ES.csv'
   var s = csv.fromPath(fileName)
   s.on('data', function (data) {
     fn(data)
   })
 }
+
+// this.processFile('Fonte de dados - Fontes.csv', console.log)
+
--- robot.js
+++ robot.js
@@ -10,17 +10,28 @@ function replace (url, codMunicipio) {
 }
 
 const cidadesFile = 'Fonte de dados - Municipios do ES.csv'
-const url = 'http://sage.saude.gov.br/graficos/morbidade/dengueEpidemiologico.php?output=json&html&ibges=<codigo_ibge>&ufs=&co_agravo=2&rm=&tc=&re_giao=&cg=&qs='
+//const url = 'http://sage.saude.gov.br/graficos/morbidade/dengueEpidemiologico.php?output=json&html&ibges=<codigo_ibge>&ufs=&co_agravo=2&rm=&tc=&re_giao=&cg=&qs='
 // const urlComCodigo = url.replace('<codigo_ibge>', codDoMunicipioF)
 
-processFile(cidadesFile, (data) => {
-  const queriedUrl = replace(url, data[0])
-  request.get(queriedUrl)
-    .end(function (err, res) {
-      if (err) console.log(err)
-      else {
-        console.log('queried url: ' + queriedUrl)
-        console.log(eval('(' + res.text + ')'))
-      }
-    })
-})
+function doQueries (url) {
+  processFile(cidadesFile, (data) => {
+    const queriedUrl = replace(url, data[0])
+    request.get(queriedUrl)
+      .end(function (err, res) {
+        if (err) console.log(err)
+        else {
+          console.log('queried url: ' + queriedUrl)
+          console.log(eval('(' + res.text + ')'))
+        }
+      })
+  })
+}
+
+function doUrls () {
+  processFile('Fonte de dados - Fontes.csv', (data) => {
+    console.log(data[6])
+    doQueries(data[6])
+  })
+}
+
+doUrls()
